5bdfdb938b6c48191de4f2f60895cb328a6eb577
Ms.tasky <mstasky@pop-os.localdomain>
2024-09-27 21:32:33 +0300

    add login with facebook

--- webapp/auth/__init__.py
+++ webapp/auth/__init__.py
@@ -1,6 +1,7 @@
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_login import AnonymousUserMixin
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 
 
 
@@ -24,6 +25,10 @@ def load_user(userid):
 def auth_create_module(app,**kwargs):
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    facebook_blueprint = make_facebook_blueprint(
+    client_id=app.config.get("FACEBOOK_CLIENT_ID"),
+    client_secret=app.config.get("FACEBOOK_CLIENT_SECRET"),
+    )
     from .view import auth_blueprint
     app.register_blueprint(auth_blueprint)
-
+    app.register_blueprint(facebook_blueprint, url_prefix="/auth/login")
