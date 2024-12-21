e52be9a2f8c8dcd65d9fb5d9f5f3e9c6590be030
Ahed Eisa <78013422+be-great@users.noreply.github.com>
2024-12-21 22:08:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = '736670cb10a600b695a55839ca3a5aa54a7d7356cdef815d2ad6e19a2031182b'
     UPLOAD_FOLDER = 'webapp/static/images/'
-    FACEBOOK_CLIENT_ID = os.environ["FACEBOOK_CLIENT_ID"]
-    FACEBOOK_CLIENT_SECRET = os.environ["FACEBOOK_CLIENT_SECRET"]
 class ProdConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # setup mysql
