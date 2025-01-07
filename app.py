df4458e0b525213b46cdfb3e96fa26273aa46154
alaahamed1 <alaa.elzeftawy@gmail.com>
2025-01-07 19:48:36 +0200

    ??

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-
 import os
 from webapp import create_app, socketio
 
-env = 'prod' #os.environ.get('WEBAPP_ENV', 'dev') ## if is not in environment just return dev
+env = 'dev' #os.environ.get('WEBAPP_ENV', 'dev') ## if is not in environment just return dev
 app = create_app('config.%sConfig' % env.capitalize())
 
 if __name__ == '__main__':
